e1129f0be1652423594e7fc728292b7be9a21304
xtakac09 <xtakac09@stud.fit.vutbr.cz>
2025-01-06 13:53:16 +0100

    Added vertical rotation, but commented it (camera skews after horizontal rotation)

--- Assets/Scripts/CameraRotation.cs
+++ Assets/Scripts/CameraRotation.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 
 public class CameraRotation : MonoBehaviour
@@ -8,7 +9,7 @@ public class CameraRotation : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        // Horizontal
         while (Input.GetKey("a"))
         {
             transform.RotateAround(center, Vector3.up, rotationSpeed);
@@ -17,8 +18,30 @@ public class CameraRotation : MonoBehaviour
         
         while (Input.GetKey("d")) 
         {
-            transform.RotateAround(center, Vector3.up, -rotationSpeed);
+            transform.RotateAround(center, Vector3.down, rotationSpeed);
             break;
         }
+        
+        // Vertical
+        // Vector3 cameraRotation = Camera.main.transform.rotation.eulerAngles;
+        //
+        // while (Input.GetKey("w")) 
+        // {
+        //     if (cameraRotation.x < 345)
+        //     {
+        //         transform.RotateAround(center, new Vector3(-1,0,0), rotationSpeed);
+        //     }
+        //     break;
+        // }
+        //
+        // while (Input.GetKey("s"))
+        // {
+        //     if (cameraRotation.x > 15)
+        //     {
+        //         transform.RotateAround(center, new Vector3(1,0,0), rotationSpeed);
+        //     }
+        //     break;
+        // }
+        
     }
 }
