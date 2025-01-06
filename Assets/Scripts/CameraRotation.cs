16c64a46c82ab1a791918e33cb86b98d603232d5
xtakac09 <xtakac09@stud.fit.vutbr.cz>
2025-01-06 12:55:20 +0100

    Basic camera rotation around Y axis

--- Assets/Scripts/CameraRotation.cs
+++ Assets/Scripts/CameraRotation.cs
@@ -1,18 +1,23 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class CameraRotation : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public float rotationSpeed = 0.2f;
 
     // Update is called once per frame
     void Update()
     {
         
+        while (Input.GetKey("a"))
+        {
+            transform.Rotate(0, -rotationSpeed, 0);  
+            break; 
+        }
+        
+        while (Input.GetKey("d")) 
+        {
+            transform.Rotate(0, rotationSpeed, 0); 
+            break;
+        }
     }
 }
